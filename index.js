c33067905ac575c9d363de2bb5b010f1a5287f24
umanand triapthi <umanandblog@gmail.com>
2022-04-22 14:30:46 +0530

    final

---
+++ index.js
@@ -0,0 +1,112 @@
+document.querySelector('.monthly').addEventListener('click', function(){
+
+    document.querySelector('.curve-time-1').textContent = '103hrs';
+    document.querySelector('.curve-lastWeek-1').textContent = 'Last Month - 128hrs';    
+    document.querySelector('.curve-time-2').textContent = '23hrs';
+    document.querySelector('.curve-lastWeek-2').textContent = 'Last Month - 29hrs';    
+    document.querySelector('.curve-time-3').textContent = '13hrs';
+    document.querySelector('.curve-lastWeek-3').textContent = 'Last Month - 19hrs';
+    document.querySelector('.curve-time-4').textContent = '11hrs';
+    document.querySelector('.curve-lastWeek-4').textContent = 'Last Month - 18hrs';
+    document.querySelector('.curve-time-5').textContent = '21hrs';
+    document.querySelector('.curve-lastWeek-5').textContent = 'Last Month - 23hrs';
+    document.querySelector('.curve-time-6').textContent = '7hrs';
+    document.querySelector('.curve-lastWeek-6').textContent = 'Last Month - 11hrs';   
+})
+
+document.querySelector('.daily').addEventListener('click', function(){
+
+    document.querySelector('.curve-time-1').textContent = '5hrs';
+    document.querySelector('.curve-lastWeek-1').textContent = 'Yesterday - 7hrs';    
+    document.querySelector('.curve-time-2').textContent = '1hrs';
+    document.querySelector('.curve-lastWeek-2').textContent = 'Yesterday - 2hrs';    
+    document.querySelector('.curve-time-3').textContent = '0hrs';
+    document.querySelector('.curve-lastWeek-3').textContent = 'Yesterday - 1hrs';
+    document.querySelector('.curve-time-4').textContent = '1hrs';
+    document.querySelector('.curve-lastWeek-4').textContent = 'Yesterday - 1hrs';
+    document.querySelector('.curve-time-5').textContent = '1hrs';
+    document.querySelector('.curve-lastWeek-5').textContent = 'Yesterday - 3hrs';
+    document.querySelector('.curve-time-6').textContent = '0hrs';
+    document.querySelector('.curve-lastWeek-6').textContent = 'Yesterday - 1hrs';
+    
+
+})
+
+
+document.querySelector('.weekly').addEventListener('click', function(){
+
+    document.querySelector('.curve-time-1').textContent = '32hrs';
+    document.querySelector('.curve-lastWeek-1').textContent = 'Last Week - 36hrs';    
+    document.querySelector('.curve-time-2').textContent = '10hrs';
+    document.querySelector('.curve-lastWeek-2').textContent = 'Last Week - 8hrs';    
+    document.querySelector('.curve-time-3').textContent = '4hrs';
+    document.querySelector('.curve-lastWeek-3').textContent = 'Last Week - 7hrs';
+    document.querySelector('.curve-time-4').textContent = '4hrs';
+    document.querySelector('.curve-lastWeek-4').textContent = 'Last Week - 5hrs';
+    document.querySelector('.curve-time-5').textContent = '5hrs';
+    document.querySelector('.curve-lastWeek-5').textContent = 'Last Week - 10hrs';
+    document.querySelector('.curve-time-6').textContent = '2hrs';
+    document.querySelector('.curve-lastWeek-6').textContent = 'Last Week - 2hrs';
+    
+
+
+})
+
+
+document.querySelector('.monthlyy').addEventListener('click', function(){
+
+    document.querySelector('.curve-time-1').textContent = '103hrs';
+    document.querySelector('.curve-lastWeek-1').textContent = 'Last Month - 128hrs';    
+    document.querySelector('.curve-time-2').textContent = '23hrs';
+    document.querySelector('.curve-lastWeek-2').textContent = 'Last Month - 29hrs';    
+    document.querySelector('.curve-time-3').textContent = '13hrs';
+    document.querySelector('.curve-lastWeek-3').textContent = 'Last Month - 19hrs';
+    document.querySelector('.curve-time-4').textContent = '11hrs';
+    document.querySelector('.curve-lastWeek-4').textContent = 'Last Month - 18hrs';
+    document.querySelector('.curve-time-5').textContent = '21hrs';
+    document.querySelector('.curve-lastWeek-5').textContent = 'Last Month - 23hrs';
+    document.querySelector('.curve-time-6').textContent = '7hrs';
+    document.querySelector('.curve-lastWeek-6').textContent = 'Last Month - 11hrs';
+    
+     
+
+})
+
+
+
+document.querySelector('.dailyy').addEventListener('click', function(){
+
+    document.querySelector('.curve-time-1').textContent = '5hrs';
+    document.querySelector('.curve-lastWeek-1').textContent = 'Yesterday - 7hrs';    
+    document.querySelector('.curve-time-2').textContent = '1hrs';
+    document.querySelector('.curve-lastWeek-2').textContent = 'Yesterday - 2hrs';    
+    document.querySelector('.curve-time-3').textContent = '0hrs';
+    document.querySelector('.curve-lastWeek-3').textContent = 'Yesterday - 1hrs';
+    document.querySelector('.curve-time-4').textContent = '1hrs';
+    document.querySelector('.curve-lastWeek-4').textContent = 'Yesterday - 1hrs';
+    document.querySelector('.curve-time-5').textContent = '1hrs';
+    document.querySelector('.curve-lastWeek-5').textContent = 'Yesterday - 3hrs';
+    document.querySelector('.curve-time-6').textContent = '0hrs';
+    document.querySelector('.curve-lastWeek-6').textContent = 'Yesterday - 1hrs';
+
+})
+
+
+document.querySelector('.weeklyy').addEventListener('click', function(){
+
+    document.querySelector('.curve-time-1').textContent = '32hrs';
+    document.querySelector('.curve-lastWeek-1').textContent = 'Last Week - 36hrs';    
+    document.querySelector('.curve-time-2').textContent = '10hrs';
+    document.querySelector('.curve-lastWeek-2').textContent = 'Last Week - 8hrs';    
+    document.querySelector('.curve-time-3').textContent = '4hrs';
+    document.querySelector('.curve-lastWeek-3').textContent = 'Last Week - 7hrs';
+    document.querySelector('.curve-time-4').textContent = '4hrs';
+    document.querySelector('.curve-lastWeek-4').textContent = 'Last Week - 5hrs';
+    document.querySelector('.curve-time-5').textContent = '5hrs';
+    document.querySelector('.curve-lastWeek-5').textContent = 'Last Week - 10hrs';
+    document.querySelector('.curve-time-6').textContent = '2hrs';
+    document.querySelector('.curve-lastWeek-6').textContent = 'Last Week - 2hrs';
+    
+
+
+})
